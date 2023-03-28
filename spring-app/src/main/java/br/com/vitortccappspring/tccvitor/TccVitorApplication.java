bb4a99087b3cd1667f06b4639a931e40f68296aa
Vitor Lucas <vitorcrispim@yahoo.com.br>
2023-03-28 16:09:40 -0300

    Retirando o feign client do projeto de spring

--- spring-app/src/main/java/br/com/vitortccappspring/tccvitor/TccVitorApplication.java
+++ spring-app/src/main/java/br/com/vitortccappspring/tccvitor/TccVitorApplication.java
@@ -1,16 +1,11 @@
 package br.com.vitortccappspring.tccvitor;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+
 
 @SpringBootApplication
-@EnableFeignClients
-
-@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class TccVitorApplication {
 
 	public static void main(String[] args) {
